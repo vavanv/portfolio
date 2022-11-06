64fcb1fa4f8cd44105e3a1e4ddda92abf7cc2ff9
Vladimir V <vladimir.vorobiev@gmail.com>
2022-11-06 16:21:20 -0500

    add contact me

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,15 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import WorkExperience from '../components/WorkExperience';
 import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import ContactMe from '../components/ContactMe';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0
+    scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 "
+    >
       <Head>
         <title>Vladimir`s portfolio</title>
       </Head>
@@ -23,12 +28,15 @@ const Home: NextPage = () => {
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
-      {/* Skills */}
       <section id="skills" className="snap-start">
         <Skills />
       </section>
-      {/* Projects */}
-      {/* Contact Me */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 };
