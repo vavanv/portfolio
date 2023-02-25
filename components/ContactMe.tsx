267323ffaa5b4eb0dc61a3b2d742638b48029c6e
Vladimir V <vladimir.vorobiev@gmail.com>
2023-02-25 18:59:31 -0500

    fix typo

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -24,8 +24,8 @@ function ContactMe({ pageInfo }: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Contact</h3>
       <div className="flex flex-col space-y-10">
         <h4 className="text-4xl font-semibold text-center">
-          I have got what you need. Let us Talk.{' '}
-          <span className="decoration-[#f7ab0a]/50 underline">Lets Talk.</span>
+          I have got what you need.{' '}
+          <span className="decoration-[#f7ab0a]/50 underline">Let&apos;s Talk.</span>
         </h4>
         <div className="space-y-10">
           <div className="flex space-x-5  justify-center">
